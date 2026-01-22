46c07034583987a6a641dedc59046dc7cffc8ffc
Darkoh <lukekirk05@gmail.com>
2026-01-22 04:02:11 +0000

    Added VAO to store settings of objects in GPU memory (Makes it easier to have multiple object states)

--- Clove/Renderer.cpp
+++ Clove/Renderer.cpp
@@ -7,6 +7,13 @@ const char* vertexShaderSource = "#version 330 core\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
 
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
+
 float vertices[] =
 {
 	-0.5f, -0.5f, 0.0f,
@@ -20,14 +27,45 @@ void renderer(GLFWwindow* window)
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	unsigned int VBO;
-	unsigned int vertexShader;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	unsigned int shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glUseProgram(shaderProgram);
+
+	// These are no longer needed
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	int vertextLocation = 0;
+	int vertexSize = 3;
+	int stride = 3; // The space  between consectuive vertex attributes
+	glVertexAttribPointer(vertextLocation, vertexSize, GL_FLOAT, GL_FALSE, stride * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glUseProgram(shaderProgram);
+	glBindVertexArray(VAO);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	
 
 	/// Check if there are any rendering issues during early development
 	/*int  success;
--- Clove/Renderer.h
+++ Clove/Renderer.h
@@ -1,14 +1,26 @@
 #pragma once
 #include "Clove.h"
 
-/// \brief The Main Method that Handles Vertex & Shaders
+/// \brief The Main Method that Handles Vertex, Fragments & Shaders 
 void renderer(GLFWwindow* window);
 
 /// \brief Stores Vertices in GPU Memory </summary>
 extern unsigned int VBO;
 
+///  \brief Stores the setup settings for objects in GPU Memory (Vertex Positions, Colours, Etc.)
+extern unsigned int VAO;
+
 /// \brief Vertex Shader Source Code Written in GLSL
 extern const char* vertexShaderSource;
 
-/// \brief Shader Object
+/// \brief Vertex Shader Object
 extern unsigned int vertexShader;
+
+/// \brief Fragment Shader Source Code Written in GLSL
+extern const char* fragmentShaderSource; 
+
+/// \brief Fragment Shader Object
+extern unsigned int fragmentShader;
+
+/// \brief Combine Vertex & Fragment Shaders to Render Objects
+extern unsigned int shaderProgram;
