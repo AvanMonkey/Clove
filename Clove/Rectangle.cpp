064ad7627cb82a3d6a524707922ffef813a6a7d0
Darkoh <lukekirk05@gmail.com>
2026-03-15 20:51:26 +0000

    Added CMake Files for Linux Compatability and Added better console messages for easier testing

--- Clove/Rectangle.cpp
+++ Clove/Rectangle.cpp
@@ -24,14 +24,24 @@ void Rectangle::updatePosition(float deltaTime)
     float springForce = x * -getSprintConstant(); // Hooke's Law
 
     netForce = netForce + springForce + decayValue;
-
-    printf("%f Force on Rectangle\n", netForce);
-
     float acceleration = netForce / getMass(); // a = F/m
-
+    float velocityIncrement = acceleration * deltaTime; // This is just here for testing to make sure out velocity calculation is accurate to the equation.
+   
     updateVelocity(acceleration, deltaTime); // v = a * t
+    updateDisplacement(velocity, deltaTime); // Displacement adds the velocity * deltaTime
 
-    updateDisplacement(velocity, deltaTime);
+    // Print for testing calculation accuracy
+
+    printf("Velocity Equation Values\n\n");
+    printf("%f Mass of the Platform\n\n", getMass());
+    printf("%f Acceleration of the Platform\n\n", acceleration);
+    printf("%f Delta Time\n\n", deltaTime);
+    printf("%f Velocity increment of Platform\n\n\n", velocityIncrement);
+
+    printf("Hooke's Law Equation Values\n\n");
+    printf("%f Net Force on Platform\n\n", netForce);
+    printf("%f Spring Constant of the Platform\n\n", getSprintConstant());
+    printf("%f Displacement of the Platform\n\n\n\n\n", x);
 
     for (int i = 1; i < vertices.size(); i += 3) {
         verticesOfObject[i] = getOriginalCoordinates()[i] - x;
@@ -41,12 +51,15 @@ void Rectangle::updatePosition(float deltaTime)
     ArrayObject.bindArray();
     BufferObject.bindBuffer(GL_ARRAY_BUFFER);
     ElementBufferObject.bindBuffer(GL_ELEMENT_ARRAY_BUFFER);
+
+    // Code adapted from LearnOpenGL (2026)
     glEnableVertexAttribArray(0);
 
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_DYNAMIC_DRAW);
     glVertexAttribPointer(vertexLocation, vertexSize, GL_FLOAT, GL_FALSE, stride * sizeof(float), byteOffset);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_DYNAMIC_DRAW);
     glEnableVertexAttribArray(vertexLocation);
+    // End of adapted code
 };
 
 bool Rectangle::findSquaresTouching(Square* square)
